afe23ef3e7faa6407c9f73ba0ce4de8b13fe17a4
Nadeem Bitar <nadeem@gmail.com>
2016-01-09 22:51:42 -0800

    Add inline style support to segments

--- src/elements/Segment.jsx
+++ src/elements/Segment.jsx
@@ -6,6 +6,6 @@ export default class Segment extends Component {
   render() {
     const classes = classNames('ui', 'segment', this.props.className)
 
-    return <div className={classes}>{this.props.children}</div>
+    return <div className={classes} style={this.props.style}>{this.props.children}</div>
   }
 }
