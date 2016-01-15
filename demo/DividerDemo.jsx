b6ecd6d820bd0a705540f420cd516096753eba0e
Nadeem Bitar <nadeem@gmail.com>
2016-01-15 06:59:41 -0800

    Support all divider variations
    closes #3

--- demo/DividerDemo.jsx
+++ demo/DividerDemo.jsx
@@ -55,6 +55,51 @@ export default class ButtonDemo extends Component {
             Take a look at <a href='https://github.com/Semantic-Org/Semantic-UI/issues/2918'>issue 2918</a>
           </Message>
         </Segment>
+
+        <Segment inverted>
+          <h3>Inverted dividers</h3>
+          <p>
+            First paragraph
+          </p>
+          <Divider inverted />
+          <Divider element='h4' horizontal inverted>Horizontal</Divider>
+        </Segment>
+
+        <Segment fitted>
+          <h3>Fitted Divider</h3>
+          <span className='content'>
+            Genji was famous and life was secure and peaceful. His ladies had in their several ways made their own lives and were happy.
+            There was an exception, Tamakazura, who faced a new crisis and was wondering what to do next.
+          <Divider fitted/>
+            She was not as genuinely frightened of him, of course, as she had been of the Higo man; but since few people could possibly know what had happened,
+            she must keep her disquiet to herself, and her growing sense of isolation. Old enough to know a little of the world, she saw more than ever what a
+            handicap it was not to have a mother.
+          </span>
+
+          <Message warning>
+            <p>
+              You need to wrap the text inside the header with {"<span class='content'>"} element otherwise React's inserted span wrappers would break Semantic UI's css.
+            </p>
+
+            Take a look at <a href='https://github.com/Semantic-Org/Semantic-UI/issues/2918'>issue 2918</a>
+          </Message>
+        </Segment>
+
+        <Segment section>
+          <h3>Section Divider</h3>
+          <p>
+            Genji was famous and life was secure and peaceful. His ladies had in their several ways made their own lives and were happy.
+            There was an exception, Tamakazura, who faced a new crisis and was wondering what to do next.
+          </p>
+          <Divider section/>
+          <h3>Section two</h3>
+          <p>
+            She was not as genuinely frightened of him, of course, as she had been of the Higo man; but since few people could possibly know what had happened,
+            she must keep her disquiet to herself, and her growing sense of isolation. Old enough to know a little of the world, she saw more than ever what a
+            handicap it was not to have a mother.
+          </p>
+        </Segment>
+
       </div>
     )
   }
--- src/elements/Divider.jsx
+++ src/elements/Divider.jsx
@@ -1,18 +1,26 @@
 import React, { Component, PropTypes } from 'react'
 import classNames from 'classnames'
 
+import propsToClasses from '../util/propsToClasses'
+
 export default class Divider extends Component {
 
   static propTypes = {
     element: PropTypes.oneOf(['h1', 'h2', 'h3', 'h4', 'h5']),
     horizontal: PropTypes.bool,
+    inverted: PropTypes.bool,
     vertical: PropTypes.bool,
-    header: PropTypes.bool
+    header: PropTypes.bool,
+    fitted: PropTypes.bool,
+    hidden: PropTypes.bool,
+    section: PropTypes.bool,
+    clearing: PropTypes.bool
   }
 
   render() {
-    const { horizontal, vertical, header, element } = this.props
-    const classes = classNames('ui', {'vertical': vertical}, {'horizontal': horizontal}, 'divider', {'header': header})
+    const { header, element } = this.props
+    const classesFromProps = propsToClasses(['vertical', 'horizontal', 'inverted', 'fitted', 'hidden', 'section', 'clearing'], this.props)
+    const classes = classNames('ui', classesFromProps, 'divider', {'header': header})
 
     if(element) {
       return <this.props.element className={classes}>{this.props.children}</this.props.element>
