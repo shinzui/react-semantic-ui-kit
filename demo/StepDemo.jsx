d0fa20e2125a2bbbce33f67f63211dd8f5b19df5
Nadeem Bitar <nadeem@gmail.com>
2016-02-01 07:04:19 -0800

    Support all Step variations and states
    closes #14

--- demo/StepDemo.jsx
+++ demo/StepDemo.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 
-import { Segment, Divider, Step, StepGroup } from '../src/index'
+import { Segment, Divider, Image,
+         Grid, Column,
+         Step, StepGroup } from '../src/index'
+
+import paragraph from './paragraph.png'
 
 const StepDemo = (props) => {
   return (
@@ -84,7 +88,6 @@ const StepDemo = (props) => {
         </StepGroup>
       </Segment>
 
-
       <Segment>
         <h3>Step States</h3>
         <StepGroup>
@@ -112,6 +115,197 @@ const StepDemo = (props) => {
 
         <StepGroup>
           <Step completed>
+            <i className='info circle icon' />
+            <div className='content'>
+              <div className='title'>Preferences</div>
+              <div className='description'>default settings</div>
+            </div>
+          </Step>
+        </StepGroup>
+      </Segment>
+
+      <Segment>
+        <Grid columns={2}>
+          <Column>
+            <StepGroup vertical fluid>
+              <Step completed>
+                <i className='info circle icon' />
+                <div className='content'>
+                  <div className='title'>Preferences</div>
+                  <div className='description'>default settings</div>
+                </div>
+              </Step>
+              <Step active>
+                <i className='info circle icon' />
+                <div className='content'>
+                  <div className='title'>Preferences</div>
+                  <div className='description'>default settings</div>
+                </div>
+              </Step>
+            </StepGroup>
+          </Column>
+          <Column>
+            <h3>Fluid Step</h3>
+          </Column>
+        </Grid>
+      </Segment>
+
+      <Segment>
+        <h3>Stackable on tablets</h3>
+        <StepGroup steps={3} ordered stackable>
+          <Step completed>
+            <div className='content'>
+              <div className='title'>Registration</div>
+              <div className='description'>email and password</div>
+            </div>
+          </Step>
+          <Step completed>
+            <div className='content'>
+              <div className='title'>Profile</div>
+              <div className='description'>name and contact</div>
+            </div>
+          </Step>
+          <Step active>
+            <div className='content'>
+              <div className='title'>Preferences</div>
+              <div className='description'>default settings</div>
+            </div>
+          </Step>
+        </StepGroup>
+      </Segment>
+
+      <Segment>
+        <h3>Evenly divided</h3>
+        <StepGroup steps={3} ordered>
+          <Step completed>
+            <div className='content'>
+              <div className='title'>Registration</div>
+              <div className='description'>email and password</div>
+            </div>
+          </Step>
+          <Step completed>
+            <div className='content'>
+              <div className='title'>Profile</div>
+              <div className='description'>name and contact</div>
+            </div>
+          </Step>
+          <Step active>
+            <div className='content'>
+              <div className='title'>Preferences</div>
+              <div className='description'>default settings</div>
+            </div>
+          </Step>
+        </StepGroup>
+
+        <Divider />
+        <StepGroup steps={4} ordered>
+          <Step completed>
+            <div className='content'>
+              <div className='title'>Registration</div>
+              <div className='description'>email and password</div>
+            </div>
+          </Step>
+          <Step completed>
+            <div className='content'>
+              <div className='title'>Profile</div>
+              <div className='description'>name and contact</div>
+            </div>
+          </Step>
+          <Step active>
+            <div className='content'>
+              <div className='title'>Preferences</div>
+              <div className='description'>default settings</div>
+            </div>
+          </Step>
+
+          <Step>
+            <div className='content'>
+              <div className='title'>Team</div>
+              <div className='description'>team settings</div>
+            </div>
+          </Step>
+        </StepGroup>
+      </Segment>
+
+      <Segment>
+        <h3>Attached</h3>
+        <StepGroup steps={3} ordered attached='top'>
+          <Step completed>
+            <div className='content'>
+              <div className='title'>Registration</div>
+              <div className='description'>email and password</div>
+            </div>
+          </Step>
+          <Step completed>
+            <div className='content'>
+              <div className='title'>Profile</div>
+              <div className='description'>name and contact</div>
+            </div>
+          </Step>
+          <Step active>
+            <div className='content'>
+              <div className='title'>Preferences</div>
+              <div className='description'>default settings</div>
+            </div>
+          </Step>
+        </StepGroup>
+
+        <Segment attached='both'>
+          <Image src={paragraph} style={{opacity: '0.5'}} />
+        </Segment>
+
+        <StepGroup steps={3} ordered attached='bottom'>
+          <Step completed>
+            <div className='content'>
+              <div className='title'>Registration</div>
+              <div className='description'>email and password</div>
+            </div>
+          </Step>
+          <Step completed>
+            <div className='content'>
+              <div className='title'>Profile</div>
+              <div className='description'>name and contact</div>
+            </div>
+          </Step>
+          <Step active>
+            <div className='content'>
+              <div className='title'>Preferences</div>
+              <div className='description'>default settings</div>
+            </div>
+          </Step>
+        </StepGroup>
+      </Segment>
+
+
+      <Segment>
+        <h3>Sizes</h3>
+        <StepGroup size='small'>
+          <Step completed>
+            <i className='info circle icon' />
+            <div className='content'>
+              <div className='title'>Preferences</div>
+              <div className='description'>default settings</div>
+            </div>
+          </Step>
+          <Step active>
+            <i className='info circle icon' />
+            <div className='content'>
+              <div className='title'>Preferences</div>
+              <div className='description'>default settings</div>
+            </div>
+          </Step>
+        </StepGroup>
+
+        <Divider />
+        <StepGroup size='large'>
+          <Step completed>
+            <i className='info circle icon' />
+            <div className='content'>
+              <div className='title'>Preferences</div>
+              <div className='description'>default settings</div>
+            </div>
+          </Step>
+          <Step active>
             <i className='info circle icon' />
             <div className='content'>
               <div className='title'>Preferences</div>
--- src/elements/step/StepGroup.jsx
+++ src/elements/step/StepGroup.jsx
@@ -3,6 +3,8 @@ import classNames from 'classnames'
 
 import propsToClasses from '../../util/propsToClasses'
 import SemanticUiPropTypes from '../../common/SemanticUiPropTypes'
+import { attachedClasses } from '../../common/semanticClasses'
+import spellNumber from '../../util/spellNumber'
 
 import Step from './Step'
 
@@ -11,6 +13,11 @@ export default class StepGroup extends Component {
   static propTypes = {
     ordered: PropTypes.bool,
     vertical: PropTypes.bool,
+    fluid: PropTypes.bool,
+    size: PropTypes.oneOf(['small', 'large']),
+    steps: PropTypes.number,
+    attached: SemanticUiPropTypes.attached,
+    stackable: PropTypes.bool,
     children: (props, propName, componentName) => {
       const prop = props[propName]
 
@@ -26,10 +33,18 @@ export default class StepGroup extends Component {
     }
   }
 
+  stepsClasses() {
+    const { steps} = this.props
+
+    if(steps) return spellNumber(steps)
+  }
+
   render() {
-    const { className }  = this.props
-    const classesFromProps = propsToClasses(['ordered', 'vertical'], this.props)
-    const classes = classNames('ui', classesFromProps, 'steps', className)
+    const { className, size, stackable }  = this.props
+    const classesFromProps = propsToClasses(['fluid', 'ordered', 'vertical'], this.props)
+    const classes = classNames('ui', classesFromProps, this.stepsClasses(), attachedClasses(this.props),
+                               {'tablet stackable': stackable },
+                               size, 'steps', className)
 
     return <div className={classes}>{this.props.children}</div>
   }
