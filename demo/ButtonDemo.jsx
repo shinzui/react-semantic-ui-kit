558af41eede9addcbd05888be0de75e85b83d9ce
Nadeem Bitar <nadeem@gmail.com>
2016-02-20 08:44:46 -0800

    Support labeled icon buttons
    refs #1

--- demo/ButtonDemo.jsx
+++ demo/ButtonDemo.jsx
@@ -1,14 +1,28 @@
 import React, { Component } from 'react'
-import { Button } from '../src/index'
+import { Button, Segment } from '../src/index'
 
 export default class ButtonDemo extends Component {
 
   render() {
     return (
       <div>
-        <Button>Simple Button</Button>
-        <Button primary>Primary Button</Button>
-        <Button secondary>Secondary Button</Button>
+        <Segment>
+          <h3>Simple button</h3>
+          <Button>Simple Button</Button>
+        </Segment>
+        <Segment>
+          <h3>Primary and secondary buttons</h3>
+          <Button primary>Primary Button</Button>
+          <Button secondary>Secondary Button</Button>
+        </Segment>
+
+        <Segment>
+          <h3>Labeled icon</h3>
+          <Button primary labeledIcon>
+            <i className='pause icon' />
+            Pause
+          </Button>
+        </Segment>
       </div>
     )
   }
--- src/elements/Button.jsx
+++ src/elements/Button.jsx
@@ -2,6 +2,7 @@ import React, { Component, PropTypes } from 'react'
 import classNames from 'classnames'
 
 import SemanticUiPropTypes from '../common/SemanticUiPropTypes'
+import propsToClasses from '../util/propsToClasses'
 import { floatedClasses } from '../common/semanticClasses'
 
 export default class Button extends Component {
@@ -10,14 +11,15 @@ export default class Button extends Component {
     primary: PropTypes.bool,
     secondary: PropTypes.bool,
     fluid: PropTypes.bool,
-    floated: SemanticUiPropTypes.floated
+    floated: SemanticUiPropTypes.floated,
+    labeledIcon: PropTypes.bool
   }
 
   render() {
-    const { primary, secondary, fluid } = this.props
-    const classes = classNames('ui', {'primary': primary},
-                               {'secondary': secondary},  {'fluid': fluid}, floatedClasses(this.props),
-                               'button', this.props.className)
+    const { labeledIcon, className } = this.props
+    const classesFromProps = propsToClasses(['primary', 'secondary', 'fluid'], this.props)
+    const classes = classNames('ui', classesFromProps, {'labeled icon': labeledIcon}, floatedClasses(this.props),
+                               'button', className)
 
     return <button className={classes}>{this.props.children}</button>
   }
