89140616c149f4404af9d4ff8b507f11253743f5
Nadeem Bitar <nadeem@gmail.com>
2016-03-07 05:53:08 -0800

    Support relaxed grid
    refs #28

--- demo/GridDemo.jsx
+++ demo/GridDemo.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-import { Header, Grid, Column, Row } from '../src/index'
+import { Segment, Header, Grid, Column, Row } from '../src/index'
 
 export default class GridDemo extends Component {
 
@@ -8,17 +8,60 @@ export default class GridDemo extends Component {
     return (
       <div className='gridExample'>
 
-        <Header element='h2'>Grid container example</Header>
-        <Grid container>
-          <Column width={4} />
-          <Column width={4} />
-          <Column width={4} />
-          <Column width={4} />
-          <Column width={4} />
-          <Column width={4} />
-          <Column width={4} />
-          <Column width={4} />
+        <Segment>
+          <Header element='h2'>Grid container example</Header>
+          <Grid container>
+            <Column width={4} />
+            <Column width={4} />
+            <Column width={4} />
+            <Column width={4} />
+            <Column width={4} />
+            <Column width={4} />
+            <Column width={4} />
+            <Column width={4} />
+          </Grid>
+        </Segment>
+
+        <Segment>
+        <h2>Relaxed grid</h2>
+        <Grid>
+          <Row columns={3}>
+            <Column />
+            <Column />
+            <Column />
+          </Row>
+
+          <Row columns={8}>
+            <Column />
+            <Column />
+            <Column />
+            <Column />
+            <Column />
+            <Column />
+            <Column />
+            <Column />
+          </Row>
+
         </Grid>
+        <Grid relaxed>
+          <Row columns={3}>
+            <Column />
+            <Column />
+            <Column />
+          </Row>
+
+          <Row columns={8}>
+            <Column />
+            <Column />
+            <Column />
+            <Column />
+            <Column />
+            <Column />
+            <Column />
+            <Column />
+          </Row>
+        </Grid>
+      </Segment>
       </div>
     )
   }
--- src/collections/grid/Grid.jsx
+++ src/collections/grid/Grid.jsx
@@ -10,12 +10,13 @@ export default class Grid extends Component {
     children: PropTypes.node,
     columns: PropTypes.number,
     page: PropTypes.bool,
-    container: PropTypes.bool
+    container: PropTypes.bool,
+    relaxed: PropTypes.bool
   }
 
   render() {
-    const { page, container, className } = this.props
-    const classes = classNames('ui', columnClasses(this.props), 'grid', {'container': page || container},  className)
+    const { page, container, relaxed, className } = this.props
+    const classes = classNames('ui', {'relaxed': relaxed}, columnClasses(this.props), 'grid', {'container': page || container},  className)
 
     return <div {...this.props} className={classes}>{this.props.children}</div>
   }
