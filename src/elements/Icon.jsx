855aec6c8554ade7be7693e1ab964a816708fc8c
Nadeem Bitar <nadeem@gmail.com>
2016-03-25 06:44:54 -0700

    Support divider icons

--- src/elements/Icon.jsx
+++ src/elements/Icon.jsx
@@ -5,9 +5,9 @@ import { lowercase, splitAndLowerCamelCase } from '../util/string'
 
 const Icon = (props) => {
 
-  const { className, type, outline, slash, square, circle } = props
+  const { className, type, outline, slash, square, circle, divider } = props
   const classes = classNames(splitAndLowerCamelCase(type), {'square': square}, {'circle': circle},
-                             {'slash': slash}, {'outline': outline}, 'icon', className)
+                             {'slash': slash}, {'outline': outline}, 'icon', className, {'divider': divider})
 
   return <i className={classes} />
 }
@@ -16,7 +16,8 @@ Icon.propTypes = {
   type: PropTypes.string.isRequired,
   outline: PropTypes.bool,
   square: PropTypes.bool,
-  slash: PropTypes.bool
+  slash: PropTypes.bool,
+  divider: PropTypes.bool
 }
 
 export default Icon
