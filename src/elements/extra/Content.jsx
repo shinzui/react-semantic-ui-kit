5b34839a05773d440b0fb24bb4c02f717ae002d0
Nadeem Bitar <nadeem@gmail.com>
2016-03-27 06:59:37 -0700

    Support vertically aligning content

--- src/elements/extra/Content.jsx
+++ src/elements/extra/Content.jsx
@@ -3,11 +3,13 @@ import React, { PropTypes } from 'react'
 import classNames from 'classnames'
 
 import propsToClasses from '../../util/propsToClasses'
+import SemanticUiPropTypes from '../../common/SemanticUiPropTypes'
+import { alignedClasses } from '../../common/semanticClasses'
 
 const Content = (props) => {
   const { className, children } = props
   const classesFromProps = propsToClasses(['extra', 'visible', 'hidden'], props)
-  const classes = classNames(classesFromProps, 'content', className)
+  const classes = classNames(alignedClasses(props), classesFromProps, 'content', className)
 
   return <div className={classes}>{children}</div>
 }
@@ -15,7 +17,8 @@ const Content = (props) => {
 Content.propTypes = {
   extra: PropTypes.bool,
   visible: PropTypes.bool,
-  hidden: PropTypes.bool
+  hidden: PropTypes.bool,
+  aligned: SemanticUiPropTypes.verticalAlignment
 }
 
 export default Content
